8c2668f92d637129c7ed078bb7999789fb55c816
Alex <friedmanalexg@gmail.com>
2022-09-14 15:42:16 -0400

    db migrated and seeded

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,11 @@
 class Cart < ApplicationRecord
-  belongs_to :client
-  has_many :cart_products
-  has_many :products, through: :cart_products
+    belongs_to :user
+    has_many :cart_products
+    has_many :products, through: :cart_products
 
-  def sum_total
-    prices = self.cart_products.map{|p| p.product.price}
-    prices.sum
-  end
+    def sum_total
+        prices = self.cart_products.map{|p| p.product.price}
+        prices.sum
+    end
+    
 end
--- db/migrate/2022091419222_create_carts.rb
+++ db/migrate/2022091419222_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :carts do |t|
       t.boolean :confirmed
-      t.belongs_to :client, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
