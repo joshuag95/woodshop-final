f3502a695203baeb0753d862bd319b4db35b0167
Alex <friedmanalexg@gmail.com>
2022-09-15 12:25:05 -0400

    updated cards

--- client/src/ProductsCards.js
+++ client/src/ProductsCards.js
@@ -8,8 +8,9 @@ const ProductsCards = ({products}) => {
         <div className= 'card'>
             
             <h2 className ='card header'>{products.name}</h2>
-            <img className = 'card image' src = {products.image} alt=""/>
-            <p className = 'card body'>{products.content}</p>
+            <h3 className = 'card price'>{products.price}</h3>
+            <img className = 'card image' src = {products.image} alt="product image"/>
+            <p className = 'card body'>{products.description}</p>
 
 
 
